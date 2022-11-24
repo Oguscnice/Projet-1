770576b307de5b2eaf4513078c5574a4935e97f9
Oguscnice <guillaume_cometto@hotmail.fr>
2022-11-24 13:16:03 +0100

    adding real menu burger for all pages

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
+const icone = document.querySelector('.navbar-mobile i');
+console.log(icone);
+
+icone.addEventListener('click', function(){
+    console.log("icône cliquée");
+    menuburgerback.classList.toggle('change-menuburgerback');
+    icone.classList.toggle('fa-times');
+});
+
+const menuburgerback = document.querySelector('.menuburgerback');
+
 var angle = 0;function carrousel(sign){
 
     spinner=document.querySelector(".carrousel");
